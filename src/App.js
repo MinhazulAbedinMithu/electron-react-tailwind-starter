c2cb68d7598b040cf9731cb314fe4bd3bd10e0a2
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2024-07-27 21:44:23 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="h-screen flex items-center justify-center bg-gray-100">
+    <h1 className="text-3xl font-bold underline text-blue-600">
+      Hello, Electron + React + Tailwind CSS!
+    </h1>
+  </div>
+);
+
+export default App;
